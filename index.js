7d4b2c07a13d0fdd9e6473e0bd72f3990194b6f4
vinayak-pc <vinayak.satapute@gmail.com>
2021-06-20 21:36:17 +0800

    v1.1 for pipeline test!

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V8.0!')
-  res.send('CICD App Direct Edit V7.0!')
+  res.send('CICD App V1.1!')
 })
 
 app.get('/status', (req, res) => {
