bd644cfb070ae0240fa960d959b913909d8919a3
vinayak-pc <vinayak.satapute@gmail.com>
2021-06-20 20:08:09 +0800

    final try!

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
   res.send('CICD App V8.0!')
-=======
   res.send('CICD App Direct Edit V7.0!')
->>>>>>> 04a51b0f200d77853bc93153838da205f186939e
 })
 
 app.get('/status', (req, res) => {
