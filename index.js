b4bbc74259a9d92ba4a849a1432a6548c323720a
vinayak-pc <vinayak.satapute@gmail.com>
2021-06-20 17:51:23 +0800

    V6.0 Changes!

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V5.0!')
+  res.send('CICD App V6.0!')
 })
 
 app.get('/status', (req, res) => {
